47d359ce14c5951d255f78d14fd32f734e9afe9b
Hiroki Takano <midwhite.development@gmail.com>
2024-03-20 23:32:56 +0900

    complete variables

---
+++ variables/README.md
@@ -0,0 +1,3 @@
+## 参照
+
+- https://doc.rust-jp.rs/book-ja/ch03-02-data-types.html
--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
-    println!("Hello, world!");
+    // declaring variable
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
+    }
+
+    println!("The value of x is: {}", x);
+
+    // tuple
+    let tup = (500, 6.4, 1);
+
+    let (_x, y, _z) = tup;
+
+    println!("The value of y is: {}", y);
+
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+
+    let six_point_four = x.1;
+
+    let one = x.2;
+
+    println!("The values are: {}, {}, {}", five_hundred, six_point_four, one);
 }
