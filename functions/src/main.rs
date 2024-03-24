38ab6f02b566b6d90aeb2d0d53e792875b965132
Hiroki Takano <midwhite.development@gmail.com>
2024-03-24 23:49:58 +0900

    complete to implement functions project

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    another_function(123);
 
-    another_function();
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {}", y);
+
+    let num = five();
+
+    println!("The num is: {}", num);
 }
 
-fn another_function() {
-    println!("Another function.");
+fn another_function(x: i32) {
+    println!("Another function. arg: {}", x);
+}
+
+fn five() -> i32 {
+    5
 }
