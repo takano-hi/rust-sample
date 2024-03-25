3c23b9062a8737e6d42d50b03a61a786f6f992a0
Hiroki Takano <midwhite.development@gmail.com>
2024-03-26 03:35:29 +0900

    complete to implement ownerships

--- ownerships/src/main.rs
+++ ownerships/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+    let mut s3 = String::from("hoge");
+
+    takes_ownership(s1.clone());
+    borrow_ownership(&s2);
+    change(&mut s3);
+
+    println!("s1: {}", s1);
+    println!("s2: {}", s2);
+    println!("s3: {}", s3);
+}
+
+fn takes_ownership(s: String) {
+    println!("{}", s);
+}
+
+fn borrow_ownership(s: &String) {
+    println!("{}", s);
+}
+
+fn change(s: &mut String) {
+    s.push_str(" & fuga");
 }
