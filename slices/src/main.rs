d173338e140c92693449be3e91b6acc9c7130672
Hiroki Takano <midwhite.development@gmail.com>
2024-04-07 13:02:22 +0900

    complete to implement slice project

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    for num in 5..8 {
+        println!("{}", num);
+    }
+
+    let s = String::from("hello, world.");
+
+    first(&s);
+}
+
+fn first(s: &String) {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        println!("{}, {}", i, item);
+    }
 }
